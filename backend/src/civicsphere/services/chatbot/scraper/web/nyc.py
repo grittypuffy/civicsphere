49d0ec8bdfaed5a20cbab912f22b1199a4e85b39
Keerthana <grittypuffy@riseup.net>
2025-11-17 22:59:03 +0530

    [feat]: poll site scraping for NYC address

--- backend/src/civicsphere/services/chatbot/scraper/web/nyc.py
+++ backend/src/civicsphere/services/chatbot/scraper/web/nyc.py
@@ -1,12 +1,30 @@
 import aiohttp
 import requests
+import usaddress
 from src.civicsphere.constants.scraper import NYC_POLLSITE_INFO_HEADERS as headers
 from src.civicsphere.constants.scraper import NYC_POLLSITE_INFO_URL as url
 
+def parse_address(street_address: str):
+    parsed, address_type = usaddress.tag(street_address)
+    number = parsed.get("AddressNumber", "")
+    name = parsed.get("StreetName", "")
+    post_type = parsed.get("StreetNamePostType", "")
+    post_dir = parsed.get("StreetNamePostDirectional", "")
+    postal_code = parsed.get("ZipCode", "")
 
-async def get_pollsite_info(*, county: str, streetnumber: str, streetname: str, postalcode: str):
+    full_street = " ".join(part for part in [name, post_type, post_dir] if part).strip()
+    if not (number and full_street and postal_code):
+        raise Exception("Improperly formatted US address")
+
+    return {
+        "streetnumber": number,
+        "streetname": full_street,
+        "postalcode": postal_code
+    }
+
+
+async def get_pollsite_info(*, streetnumber: str, streetname: str, postalcode: str):
     params = {
-        "county": county,
         "streetnumber": streetnumber,
         "streetname": streetname,
         "postalcode": postalcode,
