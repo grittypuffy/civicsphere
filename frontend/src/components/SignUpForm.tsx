ce4ce97e50982baca79c64968ba2f44b865e5040
vg006 <devvg006@gmail.com>
2025-11-22 16:28:46 +0530

    fix(page): Import router instance for client-side navigation

--- frontend/src/components/SignUpForm.tsx
+++ frontend/src/components/SignUpForm.tsx
@@ -13,11 +13,12 @@ import {
   Spinner
 } from '@fluentui/react-components';
 import { EyeOffRegular, EyeRegular } from '@fluentui/react-icons';
-import Router from 'next/router';
+import { useRouter } from 'next/navigation';
 import { useState } from "react";
 import * as v from 'valibot';
 
 const SignUpForm = ({ ToastMessage }: { ToastMessage: ToastFunc }) => {
+  const router = useRouter();
   const [formData, setFormData] = useState<SignUpFormData>({
     username: '',
     email: '',
@@ -115,7 +116,7 @@ const SignUpForm = ({ ToastMessage }: { ToastMessage: ToastFunc }) => {
               { message: 'Sign Up Successful', description: 'You can now sign in.' },
               'success'
             );
-            Router.push('/auth?action=onboard');
+            router.push('/auth?action=onboard');
             break;
           case 409:
             ToastMessage(
